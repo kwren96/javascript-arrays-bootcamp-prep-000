b281c2a7c615ab284d101a9f8154481ec96dc360
Katherine Wren <kwren@outlook.com>
2019-03-26 14:17:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,5 +21,7 @@ function accessElementInArray(array) {return array[index]}
 function destructivelyRemoveElementFromBeginningOfArray(array) { array.shift(); return array}
 function removeElementFromBeginningOfArray(array) {return array.slice(1)}
 function destructivelyRemoveElementFromEndOfArray(array) {array.pop(); return array}
+function removeElementFromEndOfArray(array) {return array.slice(0,array.length -1)}
+
 
 
