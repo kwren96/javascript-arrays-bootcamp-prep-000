bc663ed390b10aa7d11bb6bd8a043c5f76ea7e6f
Katherine Wren <kwren@outlook.com>
2019-03-26 13:47:59 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var ingredient1 = "bread";
+var ingredient22 = "mild cheese";
+var ingredient3 = 'sharp cheese';
+var ingredient4 = "butter";
+var ingredient5 = "tomato";
+var ingredient6 = "garlic";
+var grilledCheeseIngredients = ['bread', 'mild cheese', ' sharp cheese', 'butter', 'tomato', 'garlic'];
+var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 'basil', 'oregano'];
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var element = 'foo';
+
+function addElementToBeginningOfArray(array) {return [element, ...array]}
+
+var element = 'foo';
+
+function destructivelyAddElementToBeginningOfArray(array) {return array.unshift(element)}
+
+
